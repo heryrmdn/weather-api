b53dbf7002e272ed9ea22000d8a66f1b6f75d1fd
heryrmdn <heryrhamadan@gmail.com>
2025-08-22 11:36:52 +0700

    add: interface for  3rd party api

---
+++ src/interfaces/weather.ts
@@ -0,0 +1,73 @@
+export interface WeatherByCoordinateRequest {
+  lat: string;
+  lon: string;
+}
+
+export interface WeatherByCityNameRequest {
+  q: string;
+}
+
+export interface WeatherByCityIdRequest {
+  id: string;
+}
+
+export interface WeatherByZipCodeRequest {
+  zip: string;
+}
+
+export interface WeatherResponse {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: Weather;
+}
+
+export interface Weather {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  rain: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
