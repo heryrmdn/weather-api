7a932cc451e5433a5838d48c85ae14a804785ea0
heryrmdn <heryrhamadan@gmail.com>
2025-08-22 09:41:55 +0700

    modify: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import routesv1 from "../routes/v1";
 const routes = express.Router();
 
-routes.use("/api/v1", routesv1);
+routes.use("/", routesv1);
 
 export default routes;
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import weatherRoutes from "./weather.routes";
 const routesv1 = express.Router();
 
-routesv1.use("/", weatherRoutes);
+routesv1.use("/api/v1", weatherRoutes);
 
 export default routesv1;
